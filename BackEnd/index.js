c5bb366bdb564b5c6c5a165d6d7deba5b7668a63
Deepak-Kumar201 <dk10112002@gmail.com>
2021-09-21 09:37:48 +0530

    Added environment Variables

---
+++ BackEnd/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+//dotenv is a module to set environment variables automaticlly from the .env file
+const envVar = require("dotenv").config().parsed;
+
+const fireBaseApp = require('firebase/app');
+
+//connecting to the firebase
+var config = {
+	apiKey: envVar.fireBase_API,
+	authDomain: envVar.fireBase_authDomain,
+	databaseURL: envVar.fireBase_DatabaseURL,
+	storageBucket: envVar.fireBase_StorageBucket
+};
+
+fireBaseApp.initializeApp(config);
+
+
+
+app.listen(5000, () => {
+	console.log("Listning to port 5000")
+})
