69a107388518407b44f2a02645b9cc28d9367282
Bhannasa <adityabhan125@gmail.com>
2021-09-21 12:28:23 +0530

    added login endpoint

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const bcrypt=require('bcryptjs');
+const jwt = require('jsonwebtoken');
 import {
     getFirestore,
     collection,
@@ -15,7 +16,7 @@ import {
     getDoc,
     serverTimestamp,
 } from 'firebase/firestore';
-
+const envVar = require("dotenv").config().parsed;
 
 router.post('/createuser',async (req,res)=>{
     try{
@@ -25,7 +26,7 @@ router.post('/createuser',async (req,res)=>{
         const cpassword = req.body.cpassword;
         const phone = req.body.phone;
 
-        const usersRef=collection(db,"users");
+        const usersRef=collection(db,"users",username);
         const userSnap=await getDoc(usersRef);
 
         if(userSnap.exists()||username.length<5||password.length<5||password!=cpassword||phone.length!=10){
@@ -34,7 +35,6 @@ router.post('/createuser',async (req,res)=>{
         const salt = await bcrypt.genSalt(10);
         const secPass = await bcrypt.hash(password,salt);
         
-        const curTime=new Date();
         await setDoc(doc(usersRef,username),{
             username:username,
             name:name,
@@ -42,9 +42,38 @@ router.post('/createuser',async (req,res)=>{
             phone:phone,
             createdOn:serverTimestamp,
             lastSeen:serverTimestamp,
-            profilePic:"",
+            profilePicUrl:"",
             settings:{},
         })
+
+        const data={
+            user:{username}
+        }
+
+        const authToken=jwt.sign(data,envVar.fireBase_API); 
+        res.json({authToken});
+
+    }catch(error){
+        res.status(500).json("Some server error occured:",error);
+    }
+})
+
+router.get('/login',async (req,res)=>{
+    try{
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const usersRef=collection(db,"users",username);
+        const userSnap=await getDoc(usersRef);
+
+        if(!userSnap.exists())   return res.status(400).json({'error':'Please register with us first'});
+        if(!bcrypt.compareSync(password,userSnap.data().password)) res.status(400).json({'error':"incorrect password"});
+
+        const data={
+            user:{username}
+        }
+        const authToken=jwt.sign(data,envVar.fireBase_API); 
+        res.json({authToken});
 
     }catch(error){
         res.status(500).json("Some server error occured:",error);
