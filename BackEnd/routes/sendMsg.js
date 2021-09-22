7118583267d0a4ee10946a34eb0fce9a99afa893
Bhannasa <adityabhan125@gmail.com>
2021-09-22 18:08:28 +0530

    added chat-contact history

--- BackEnd/routes/sendMsg.js
+++ BackEnd/routes/sendMsg.js
@@ -45,7 +45,10 @@ router.post("/sendMessage", getUserName, async (req, resp) => {
 
 		//getting user name from jwt tokken and getting the collection name 
 		let user = userData.username;
-
+		if(user === req.body.user){
+			resp.status(400).json({'error':"Can't message yourself"});
+			return;
+		}	
 		if (user < req.body.user) {
 			user += req.body.user;
 		} else user = req.body.user + user;
@@ -61,14 +64,36 @@ router.post("/sendMessage", getUserName, async (req, resp) => {
 			"reply": req.body.reply,
 			"time": dt.getTime(),
 			"type": req.body.type,  // type currently supports - 'text'
-			"newprop": (userData.username+req.body.user),
 		}
 
 		//creating message id
 		var msgID = parseInt(dt.getTime() * 10 * Math.random())
 		//created
 		database.set(database.ref(dbRealTime, `${user}/${msgID}`), msgBody);
-		resp.status(200).send("Message Send");
+
+		// dragging chat to top for both users
+		let chats=userData.chats;
+		const idx=chats.indexOf(req.body.user);
+		if(idx>=0)	chats.splice(idx,1);
+		chats.unshift(req.body.user);
+		firestore.updateDoc(ref, {
+			chats: chats
+		})
+		
+		const refU2 = firestore.doc(db, "users", req.body.user);
+		// const userDocU2 = await firestore.getDoc(refU2);
+		// const userDataU2 = userDoc.data();
+		const userDataU2 = (await firestore.getDoc(refU2)).data();
+		let chatsU2=userDataU2.chats;
+		const idxU2=chatsU2.indexOf(req.username);
+		if(idxU2>=0)	chatsU2.splice(idxU2,1);
+		chatsU2.unshift(req.username);
+		firestore.updateDoc(refU2, {
+			chats: chatsU2
+		})
+
+
+		resp.status(200).send("Message Sent");
 	} catch(error) {
 		resp.status(400).send("Something Wrong! Please try again after some time")
 	}
