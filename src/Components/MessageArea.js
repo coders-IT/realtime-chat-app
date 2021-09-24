556c9a61dfaa66cf47b874f4386bac8c637ba73a
Bhannasa <adityabhan125@gmail.com>
2021-09-24 14:42:45 +0530

    change

--- src/Components/MessageArea.js
+++ src/Components/MessageArea.js
@@ -21,7 +21,7 @@ export default function MessageArea() {
                     })
                 }
             </div>
-            {/* <SendBox/> */}
+            <SendBox/>
         </>
     )
 }
